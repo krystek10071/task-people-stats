7ce0138a7ee2144a1775dbcedd268f7268f8e4f5
Ktoś <krystek10071@wp.pl>
2020-10-25 20:17:36 +0100

    commit point 7

--- src/com/company/PeopleStats.java
+++ src/com/company/PeopleStats.java
@@ -23,4 +23,12 @@ public class PeopleStats {
     public long count() {
         return people.size();
     }
+
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
+    }
+
 }
